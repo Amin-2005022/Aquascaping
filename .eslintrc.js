f5c31f977ec65942ec98f10785b33a75e425435c
Amin-2005022 <amin.ekramul012@gmail.com>
2025-08-09 21:16:12 +0600

    Fix ESLint parsing errors for TypeScript/JSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
-// This file contains JSX-specific ESLint rule overrides
+// This file contains additional ESLint configurations
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
-    'react/jsx-no-undef': 'off'
+    'react/jsx-no-undef': 'off',
+    'react/no-unescaped-entities': 'off'
   }
 };
